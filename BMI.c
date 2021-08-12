916a802d6d8381bd9d830d3ec6c3b6602ba2f6e0
Im9J <64010327@kmitl.ac.th>
2021-08-12 22:25:05 +0700

    Create BMI.c

---
+++ BMI.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <Math.h>
+
+int main () {
+	double Weight,Height,BMI;
+	printf("Enter your weight (kg) :");
+	scanf("%lf",&Weight);
+	printf("Enter your Height (m) :");
+	scanf("%lf",&Height);
+	BMI = Weight/pow(Height,2);
+	printf("BMI = %lf\n",BMI);
+	
+	if(BMI < 18.5){
+		printf("Underweight");
+	}
+	else if(18.5 < BMI < 24.9){
+		printf("Normal");
+	}
+	else if(25.0 < BMI < 29.9){
+		printf("Overweight");
+	}
+	else if(BMI > 30){
+		printf("Obesity");
+	}
+}
